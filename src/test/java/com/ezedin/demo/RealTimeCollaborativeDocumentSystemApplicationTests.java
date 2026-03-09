7f4712eb06e6353a5b44fe50003490da87c8a78d
Ezedin Ahmed <ezedinadefa@gmail.com>
2026-03-09 15:06:26 +0300

    initializing the project

---
+++ src/test/java/com/ezedin/demo/RealTimeCollaborativeDocumentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezedin.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeCollaborativeDocumentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
